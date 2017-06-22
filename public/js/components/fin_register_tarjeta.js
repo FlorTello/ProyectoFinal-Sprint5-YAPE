8314a6d33095e28d6ee1c52ab744a7ce78f1ffb8
Flor Tello <flor.26071994@gmail.com>
2017-06-22 11:23:25 -0500

    Agregando componentes de cuenta

--- public/js/components/fin_register_tarjeta.js
+++ public/js/components/fin_register_tarjeta.js
@@ -8,12 +8,26 @@ const FinRegistroTarjeta = (update) => {
   const h5 = $('<h5>Hola</h5>');
   const enlace = $('<p class="teal-text text-accent-4"><img src="img/icons/eye.png" class="center" alt="phone" width="5%">  Mostrar Saldo</p>');
   header.append(icon_top,img,h5,enlace);
-  const div_movi = $('<p class="col s12 text-uppercase left-align">últimos movimientos</h6><img class="right" src="img/icons/right-arrow-circular-button.png" alt="" class="img-responsive" width="25px"></p>');
-
+  const div_movi = $('<p class="col s12 text-uppercase left-align">últimos movimientos</h6><img class="right" src="img/icons/right-arrow-circular-button.png" alt="" class="img-responsive" width="25px"></p><hr>');
   const icon = $('<img src="img/icons/check.png" class="center" alt="phone" width="0%">');
   const p = $('<h5 class="black-text text-darken-3"> ¡Bien! <br> Ahara puedes usar Yape </h5>');
-  // div.append(icon,p);
-  container.append(header,div_movi);
+  const info = $(`<div class="container section">
+                    <div class="row">
+                      <img src="img/icons/icon.png" class="img-responsive col s5" alt="yape" width="40%">
+                      <p class="col s7 right-align text-bold"><br><h6>¿Aún no realizas tu primer pago?</h6> es rápido y secillo</p>
+                    </div>
+                  </div><br>`);
+  const footer = $(`<div class="flex-footer purple-dark center section">
+                    <div class="container">
+                      <img src="img/icons/send.png" class="img-responsive" alt="yape">
+                      <p class="text-uppercase">Enviar Pago</p>
+                    </div>
+                    <div class="container">
+                      <img src="img/icons/code-qr.png" class="img-responsive" alt="yape">
+                      <p class="text-uppercase">Recibir pago</p>
+                    </div>
+                  </div>`);
+  container.append(header,div_movi,info,footer);
   return container
 
 };
